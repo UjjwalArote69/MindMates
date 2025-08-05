8e871baa457d1b8a3708918f6a42449150996eec
UjjwalArote369 <ujjwalarote369@gmail.com>
2025-08-05 20:23:58 +0530

    Phase 1: Typescript + Express boilerplate setup

---
+++ Backend/src/app.ts
@@ -0,0 +1,12 @@
+import e from 'express';
+import express, {Request, Response} from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello World!');
+});
+
+export default app;
