d095fe1398d86020e42ba61c40ad439a351b57fd
UjjwalArote369 <ujjwalarote369@gmail.com>
2025-09-25 22:12:31 +0530

    changes

--- Frontend/src/types/declarations.d.ts
+++ Frontend/src/types/declarations.d.ts
@@ -1,13 +1,24 @@
-declare module "swiper/css";
-declare module "swiper/css/pagination";
-// declare module "swiper/css/navigation";
 declare module "*.svg" {
-  import * as React from "react";
-  const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
-  export default ReactComponent;
+  const content: string;
+  export default content;
 }
 
-declare module "*.png";
-declare module "*.jpg";
-declare module "*.jpeg";
-declare module "*.gif";
+declare module "*.png" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.jpg" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.jpeg" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.gif" {
+  const content: string;
+  export default content;
+}
