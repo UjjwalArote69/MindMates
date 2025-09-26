dc2ed3710de00035b40964b50aabbef06dd36b5c
UjjwalArote369 <ujjwalarote369@gmail.com>
2025-09-26 10:57:57 +0530

    Cors policy issue

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -13,13 +13,26 @@ app.use(express.json());
 app.use(passport.initialize());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://mindmates-beta.vercel.app",
+  "https://mindmates-git-main-ujjwalarote69s-projects.vercel.app",
+  "https://mindmates-kl7qxai0x-ujjwalarote69s-projects.vercel.app",
+];
+
 app.use(
   cors({
-    // origin: [process.env.CLIENT_ID || "http:localhost:5173", "https://mindmates-beta.vercel.app", "https://mindmates-git-main-ujjwalarote69s-projects.vercel.app", "https://mindmates-rehmj7dmt-ujjwalarote69s-projects.vercel.app"],
-    origin: "*",
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
+
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
